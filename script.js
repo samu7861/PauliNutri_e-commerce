5e126cdf1d48a10fd415cc72a22595b479f1c2e9
samu7861 <sfrancog7861@gmail.com>
2023-07-11 12:30:27 -0500

    mobile device v2

--- script.js
+++
@@ -1,5 +0,0 @@
-// Agregar evento click al icono del menú
-document.querySelector('.menu-icon').addEventListener('click', function() {
-  // Alternar la visibilidad del menú móvil
-  document.querySelector('.mobile-menu').classList.toggle('show');
-});
